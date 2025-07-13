d9a609391db9e2e30d04fe8e69a3d99e51aad322
Anil Kaynar <anilkaynar@Anils-MacBook-Pro.local>
2025-07-13 20:08:22 +0300

    Use docker buildx for multi-platform image builds

--- GenerateDockerImage.bash
+++ GenerateDockerImage.bash
@@ -5,6 +5,6 @@ TAG=${2:-latest}
 
 echo "Building Docker image from $DOCKERFILE_PATH with tag: $TAG"
 
-docker build -f "$DOCKERFILE_PATH" -t aanilkay/minimalnextollamachat:$TAG .
+docker buildx build --platform linux/amd64,linux/arm64 -f "$DOCKERFILE_PATH" -t aanilkay/minimalnextollamachat:$TAG .
 
 docker push aanilkay/minimalnextollamachat:$TAG
